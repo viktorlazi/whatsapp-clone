a97208150fed3b4a4792b4335821f334f67e6229
Viktorlazi <viktorlazi010@gmail.com>
2021-01-23 13:34:19 +0100

    Potrosia 3 sata na rijesavanje nekog problema koji nije postoja... Ugl. Sada mogu koristit @material-ui

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <DonutLargeIcon/>
       <Chat />    
     </div>
   );
