49970c8505b0fffd1d87265bfb461ad217284d7b
Viktorlazi <viktorlazi010@gmail.com>
2021-01-23 08:50:27 +0100

    components Sidebar/Chat

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Ej</h1>
-    
+      <Sidebar />
+      <Chat />    
     </div>
   );
 }
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBIJNe4B6z4s4eDU3lQAVarZBQ-cNLicM",
+    authDomain: "whatsapp-mern-cc9b6.firebaseapp.com",
+    projectId: "whatsapp-mern-cc9b6",
+    storageBucket: "whatsapp-mern-cc9b6.appspot.com",
+    messagingSenderId: "76380157526",
+    appId: "1:76380157526:web:fdfcb2fdbd7ca18584adf7"
+  };
