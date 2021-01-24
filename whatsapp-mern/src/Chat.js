c5cf940bf39b82db45bd20032e70cb7529b6aaf8
Viktorlazi <viktorlazi010@gmail.com>
2021-01-24 09:40:34 +0100

    design finished for now, logic still missing

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -4,6 +4,8 @@ import {Avatar, IconButton} from '@material-ui/core'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import MicIcon from '@material-ui/icons/Mic'
 
 const Chat = () => {
     return (
@@ -38,7 +40,38 @@ const Chat = () => {
                     }
                   </span>
                 </p>
+                <p className="chat__message">
+                  <span className="chat__name">
+                      Viktor
+                  </span>
+                  This is a message
+                  <span className="chat__timestamp">
+                    {
+                      new Date().toUTCString()
+                    }
+                  </span>
+                </p>
+                <p className="chat__message chat__reciever">
+                  <span className="chat__name">
+                      Viktor
+                  </span>
+                  This is a message
+                  <span className="chat__timestamp">
+                    {
+                      new Date().toUTCString()
+                    }
+                  </span>
+                </p>
             </div>
+            <div className="chat__footer">
+              <InsertEmoticonIcon />
+              <form>
+                <input placeholder="Type a message" type="text" />
+                <button type="submit">Send a message</button>
+              </form>
+              <MicIcon />
+            </div>
+
         </div>
     )
 }
