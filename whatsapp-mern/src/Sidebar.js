256b457a44d6afef6fef4fa7f4da24424d69a224
Viktorlazi <viktorlazi010@gmail.com>
2021-01-24 08:25:08 +0100

    sidebar__header

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Sidebar.css'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {Avatar, IconButton} from '@material-ui/core'
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+            <div className="sidebar__header">
+                <Avatar/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
