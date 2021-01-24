0c5e41e6bef13ade35b394eb28d27b27cd2ae96e
Viktorlazi <viktorlazi010@gmail.com>
2021-01-24 09:16:38 +0100

    finished chat header, started styling messages

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import './Chat.css'
+import {Avatar, IconButton} from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const Chat = () => {
     return (
         <div className="chat">
-            <h1>Chat</h1>
+            <div className="chat__header">
+                <Avatar/>
+                <div className="chat__headerInfo">
+                    <h3>Ime Prezime</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                  <span className="chat__name">
+                      Viktor
+                  </span>
+                  This is a message
+                  <span className="chat__timestamp">
+                    {
+                      new Date().toUTCString()
+                    }
+                  </span>
+                </p>
+            </div>
         </div>
     )
 }
